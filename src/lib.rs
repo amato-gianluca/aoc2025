29bb9909761c57c532e0b750e1b3d79d31fcf6d8
Gianluca Amato <gianluca.amato.74@gmail.com>
2025-12-13 12:55:06 +0100

    Add comments and support for closures in the code reading from input files.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
-pub fn read_file<R>(filename: &str, pat: &str, parser: fn(&str)-> R) -> Vec<R>
+/// Read the file `filename`, splitting its content on the pattern `pat`. Each of the pieces
+/// is passed to the `parser` function, and it returns a vector of the elements returned by
+/// `parser`.
+pub fn read_file<P, R>(filename: &str, pat: &str, parser: P) -> Vec<R>
+where
+    P: Fn(&str) -> R,
 {
     std::fs::read_to_string(filename)
         .unwrap()
@@ -8,7 +13,14 @@ pub fn read_file<R>(filename: &str, pat: &str, parser: fn(&str)-> R) -> Vec<R>
         .collect()
 }
 
-pub fn read_file_split<R,S>(filename: &str, parser1: fn(&str)-> R, parser2: fn(&str)-> S) -> (Vec<R>, Vec<S>)
+/// Read the file `filename`, splitting its content on an empty line. The part before
+/// the empty line is fed to `parser1` one line at a time, the part before the empty line
+/// is similarly sent to `parser2`. It returns a pair of vectors, with all the results of
+/// the two parsers.
+pub fn read_file_split<P1, P2, R, S>(filename: &str, parser1: P1, parser2: P2) -> (Vec<R>, Vec<S>)
+where
+    P1: Fn(&str) -> R,
+    P2: Fn(&str) -> S,
 {
     let content = std::fs::read_to_string(filename).unwrap();
 
